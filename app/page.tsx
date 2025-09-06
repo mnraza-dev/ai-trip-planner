2eca6e5acf3d885c2f24f03f685c559b51eec84f
Md Noorullah Raza <mnraza94@gmail.com>
2025-09-06 15:21:34 +0530

    Add PopularCities component and integrate it into Home; update next.config for image handling; add @tabler/icons-react dependency

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
 import Hero from './_components/Hero'
+import { PopularCities } from './_components/PopularCities'
 
 const Home = () => {
   return (
     <div className='p-4 '>
 
       <Hero />
-
+      <PopularCities />
     </div>
   )
 }
