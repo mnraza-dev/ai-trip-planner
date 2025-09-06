377c9204aa486d0cfdc168b9837727a62b4937b5
Md Noorullah Raza <mnraza94@gmail.com>
2025-09-06 10:53:46 +0530

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,6 @@ const Home = () => {
   return (
     <div className='p-4 flex flex-col gap-4'>
 
-      <Button variant="destructive">
-        Custom Button
-      </Button>
-      <Button variant="outline">Button</Button>
 
     </div>
   )
---
+++ app/provider.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './_components/Header';
+
+const Provider = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return (
+        <div>
+            <Header />
+            {children}
+
+        </div>
+    )
+}
+
+export default Provider
